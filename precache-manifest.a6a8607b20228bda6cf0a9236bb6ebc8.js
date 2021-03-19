14ad5a6e61d62cca8eeb09279d4cb8d7e11fc9f3
Vladimir Muratov <vladimirmuratov@mail.ru>
2021-03-19 17:52:52 +0300

    Updates

---
+++ precache-manifest.a6a8607b20228bda6cf0a9236bb6ebc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb71d4bc205fd9bb58ce8926d764be5a",
+    "url": "/inernet_shop_phones/index.html"
+  },
+  {
+    "revision": "6cac5d1148e5a68e38b7",
+    "url": "/inernet_shop_phones/static/css/main.855edcfb.chunk.css"
+  },
+  {
+    "revision": "87efd08b3eff3b8302fa",
+    "url": "/inernet_shop_phones/static/js/2.cd03ecc9.chunk.js"
+  },
+  {
+    "revision": "6cac5d1148e5a68e38b7",
+    "url": "/inernet_shop_phones/static/js/main.33ba757a.chunk.js"
+  },
+  {
+    "revision": "2b4e7cc83f9941d00c5b",
+    "url": "/inernet_shop_phones/static/js/runtime~main.36088890.js"
+  }
+]);
